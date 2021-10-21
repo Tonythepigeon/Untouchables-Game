5f6a2cfe30a0dd85f11367292ad8d0ba0dd53b7b
Jimmy Truong <54197644+jqt-designs@users.noreply.github.com>
2021-10-21 14:20:34 -0600

    Game Over Screen
    After health goes down after each shot

--- untouchables.js
+++ untouchables.js
@@ -6,20 +6,36 @@ let frame = 0;
 let canvas = document.getElementById('myCanvas');
 let ctx = canvas.getContext('2d');
 let keyMap = new Object();
+
+var isGameOver;
+var score = 0;
+
+function init() {
+    document.getElementById('play-again'), addEventListener('click', function () {
+        reset();
+    });
+
+    reset();
+    fireTimer = 0;
+    main();
+}
+
+
+
 //keyboard listeners
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
     keyMap[e.key] = true;
 }
-document.onkeyup = function(e) {
+document.onkeyup = function (e) {
     keyMap[e.key] = false;
 }
-ctx.canvas.width  = window.innerWidth - 20;
+ctx.canvas.width = window.innerWidth - 20;
 ctx.canvas.height = window.innerHeight - 20;
 //init global variables
 let player = new PlayerObject(
     'mainShip.png',
     canvas.width / 2,
-    canvas.height - 80,
+    canvas.height - 100,
     STARTING_ACCELERATION,
     STARTING_FRICTION
 );
@@ -36,42 +52,47 @@ let darkBullet = new Bullet(darkBulletSprite,
     10,
     1
 );
-let enemyContainer = new EnemyContainer(["redLasor.gif", "mainShip.png"], 100,100);
+let enemyContainer = new EnemyContainer(["redLasor.gif", "mainShip.png"], 100, 100);
 
 window.requestAnimationFrame(drawGame);
 
 
 function handleKeyboardInput() {
-    if(keyMap["ArrowDown"])
+    if (keyMap["ArrowDown"] || keyMap['s'] || keyMap['S'])
         player.moveDown();
-    if(keyMap["ArrowUp"])
+    if (keyMap["ArrowUp"] || keyMap['w'] || keyMap['W'])
         player.moveUp();
-    if(keyMap["ArrowLeft"])
+    if (keyMap["ArrowLeft"] || keyMap['a'] || keyMap['A'])
         player.moveLeft();
-    if(keyMap["ArrowRight"])
+    if (keyMap["ArrowRight"] || keyMap['d'] || keyMap['D'])
         player.moveRight();
-    if(keyMap[" "]){
-        if(fireTimer == 0) {
+    if (keyMap[" "]) {
+        if (fireTimer == 0) {
             player.fireBullet();
             fireTimer++;
+            player.health--;
         }
     }
-    if(frame % (200) == 0)
-        enemyContainer.newEnemy(0,darkBullet,1,3);
+    if (frame % (200) == 0)
+        enemyContainer.newEnemy(0, darkBullet, 1, 3);
 }
 
-function drawGame(){
+function drawGame() {
     frame++;
     handleKeyboardInput();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     enemyContainer.update();
-    player.updatePlayer();
-    player.draw();
     enemyContainer.draw();
-    if(fireTimer != 0){
+    if (player.health > 0) {
+        player.updatePlayer();
+        player.draw();
+    } else {
+        gameOver();
+    }
+    if (fireTimer != 0) {
         fireTimer++;
     }
-    if(fireTimer > STARTING_FIRE_RATE){
+    if (fireTimer > STARTING_FIRE_RATE) {
         fireTimer = 0;
     }
     //requests next frame (don't call anything after)
@@ -82,16 +103,16 @@ function drawGame(){
 
 //OLD MOVE OBJECT CODE
 
-function moveObject(object, direction, distance, func){
-    if(func == "Stick on Screen"){
-        if(object.xPos > canvas.width - object.width){
+function moveObject(object, direction, distance, func) {
+    if (func == "Stick on Screen") {
+        if (object.xPos > canvas.width - object.width) {
             object.xPos = canvas.width - object.width;
-        }else if(object.xPos < 0){
+        } else if (object.xPos < 0) {
             object.xPos = 0;
         }
-        if(object.yPos > canvas.height - object.height){
+        if (object.yPos > canvas.height - object.height) {
             object.yPos = canvas.height - object.height;
-        }else if(object.yPos < 0){
+        } else if (object.yPos < 0) {
             object.yPos = 0;
         }
     }
@@ -99,13 +120,33 @@ function moveObject(object, direction, distance, func){
 }
 
 
-function detectCollision(){
-    elementsOnCanvas.forEach(function(element) {
-        elementsOnCanvas.forEach(function(movingObject) {
-            if(movingObject != element && movingObject.xPos - movingObject.width / 2 <=  element.xPos + element.width / 2 && movingObject.xPos + movingObject.width / 2 >= element.xPos - element.width / 2
-                && movingObject.yPos + movingObject.height / 2 >=  element.yPos - element.height / 2 && movingObject.yPos - movingObject.height / 2 <= element.yPos + element.height / 2){
+function detectCollision() {
+    elementsOnCanvas.forEach(function (element) {
+        elementsOnCanvas.forEach(function (movingObject) {
+            if (movingObject != element && movingObject.xPos - movingObject.width / 2 <= element.xPos + element.width / 2 && movingObject.xPos + movingObject.width / 2 >= element.xPos - element.width / 2
+                && movingObject.yPos + movingObject.height / 2 >= element.yPos - element.height / 2 && movingObject.yPos - movingObject.height / 2 <= element.yPos + element.height / 2) {
                 console.log("Colision detected between: " + movingObject.name + " and " + element.name);
             }
         });
     });
 }
+
+function gameOver() {
+    document.getElementById("game-over").style.display = "block";
+    document.getElementById("game-over-overlay").style.display = "block";
+    isGameOver = true;
+}
+
+// Reset game to original state
+function reset() {
+    document.getElementById('game-over').style.display = 'none';
+    document.getElementById('game-over-overlay').style.display = 'none';
+    isGameOver = false;
+    gameTime = 0;
+    score = 0;
+
+    enemies = [];
+    bullets = [];
+
+    player.pos = [50, canvas.height / 2]; 
+};
